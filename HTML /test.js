c138701a0dbc05368dd8ee102f2d6bb42ed789a1
Josephine Gyamera <test@TESTs-MacBook-Pro.local>
2020-02-26 01:21:49 +0200

    Styling the title with bootstrap

---
+++ HTML /test.js
@@ -0,0 +1,3 @@
+var tweet = prompt("Write something here: ");
+var tweetCount = tweet.length;
+alert("You have written "+ tweetCount + "characters, you have " + (140-tweetCount) + " characters remaining");
